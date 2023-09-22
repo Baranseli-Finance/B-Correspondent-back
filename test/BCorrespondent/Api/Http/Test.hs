e35b828ddc775ce18c4af11d9c2abcda3d46bb51
Sergey Yakovlev <fclaw007@gmail.com>
2023-09-22 10:57:57 +0300

    test

--- test/BCorrespondent/Api/Http/Test.hs
+++ test/BCorrespondent/Api/Http/Test.hs
@@ -7,10 +7,13 @@ module BCorrespondent.Api.Http.Test (spec_api) where
 
 import Data.Proxy
 import BCorrespondent.Api.Auth (AuthApi)
+import BCorrespondent.Api.Invoice (InvoiceApi)
+import BCorrespondent.Api.Frontend (FrontendApi)
 import BCorrespondent.Transport.Model.Auth (Credentials, AuthToken, NewPassword)
 import BCorrespondent.Api.Handler.SendGrid.SendMail (SendGridSendMailRequest)
 import BCorrespondent.Config (Email)
 import BCorrespondent.Api.Foreign.SendGrid (SendGridApi)
+import BCorrespondent.Transport.Model.Frontend (ProcuratoryRequest)
 
 import Servant.API.Generic
 import Servant.Swagger.Test
@@ -22,6 +25,7 @@ mkArbitrary ''Credentials
 mkArbitrary ''Email
 mkArbitrary ''SendGridSendMailRequest
 mkArbitrary ''NewPassword
+mkArbitrary ''ProcuratoryRequest
 
 spec_api :: Spec
 spec_api =
@@ -30,3 +34,7 @@ spec_api =
       validateEveryToJSON (genericApi (Proxy :: Proxy AuthApi))
     context "ToJSON matches ToSchema (SendGridApi)" $
       validateEveryToJSON (genericApi (Proxy :: Proxy SendGridApi))
+    context "ToJSON matches ToSchema (InvoiceApi)" $
+      validateEveryToJSON (genericApi (Proxy :: Proxy InvoiceApi))
+    context "ToJSON matches ToSchema (FrontendApi)" $
+      validateEveryToJSON (genericApi (Proxy :: Proxy FrontendApi))
