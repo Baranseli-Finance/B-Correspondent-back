ba3f9e4d869d9c93376ff9720b28ea1a575004de
Sergey Yakovlev <fclaw007@gmail.com>
2023-09-30 15:17:09 +0300

    src/

--- src/core/BCorrespondent/Api/Handler/Webhook/CatchGithub.hs
+++ src/core/BCorrespondent/Api/Handler/Webhook/CatchGithub.hs
@@ -2,25 +2,28 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module BCorrespondent.Api.Handler.Webhook.CatchGithub (catch) where
 
+import qualified BCorrespondent.Transport.Model.Frontend as Front
 import BCorrespondent.Transport.Model.Github (Sha (..))
 import BCorrespondent.Transport.Payload (Payload (..))
 import BCorrespondent.Transport.Response (Response (Ok))
 import Katip.Handler (KatipHandlerM, katipEnv, cache, ask)
 import Control.Lens ((^.))
-import Data.Aeson (eitherDecode, encode, Value (Object))
+import Data.Aeson (eitherDecode, encode, Value (Object), toJSON)
 import Cache (Cache (..))
 import Data.Foldable (for_)
 import Data.String (fromString)
 import BuildInfo (location)
 import Katip
 import Data.Either.Combinators (whenLeft)
+import Data.Maybe (fromMaybe)
+import Data.Functor ((<&>))
 
 catch :: Payload -> KatipHandlerM (Response ())
 catch payload = do
-  Cache {insert} <- fmap (^. katipEnv . cache) ask
   let val = 
         eitherDecode @Sha $ 
           encode $ 
@@ -30,4 +33,23 @@ catch payload = do
     $(logTM) ErrorS $ 
       fromString $ 
         $location <> ", error: " <> e
-  fmap Ok $ for_ val $ \Sha {..} -> undefined
+  fmap Ok $ for_ val $ \Sha {..} -> do
+    Cache {insert, get} <- fmap (^. katipEnv . cache) ask
+    oldXsE <- get "github" <&> 
+        fromMaybe (Right mempty) 
+      . fmap (eitherDecode . encode)
+    for_ oldXsE $ \oldXs -> do
+      let val = 
+            Front.Sha 
+            { Front.shaKey = 
+              shaRepo, 
+              Front.shaValue = 
+              shaValue
+            }
+      let newXs = toJSON $ modifySha oldXs val
+      insert "github" newXs
+
+modifySha [] val = [val]
+modifySha (x:xs) v 
+  | Front.shaKey x == Front.shaKey v = v:xs
+  | otherwise = x : modifySha xs v
