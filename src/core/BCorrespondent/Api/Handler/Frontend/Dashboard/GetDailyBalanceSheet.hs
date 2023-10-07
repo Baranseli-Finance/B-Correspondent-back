95b41e52e45f415ffa6a07f9264b744bc6024d3c
Sergey Yakovlev <fclaw007@gmail.com>
2023-10-07 12:58:24 +0300

    timeline init

--- src/core/BCorrespondent/Api/Handler/Frontend/Dashboard/GetDailyBalanceSheet.hs
+++ src/core/BCorrespondent/Api/Handler/Frontend/Dashboard/GetDailyBalanceSheet.hs
@@ -22,8 +22,9 @@ import BCorrespondent.Auth (AuthenticatedUser (..))
 import qualified BCorrespondent.Auth as Auth
 import Katip.Handler (KatipHandlerM, katipEnv, hasqlDbPool, ask)
 import Control.Monad.Time (currentTime)
-import Data.Time.Clock (UTCTime (utctDayTime), addUTCTime)
+import Data.Time.Clock (UTCTime (..), addUTCTime)
 import Data.Time.LocalTime (todMin, timeToTimeOfDay)
+import Data.Time.Calendar.OrdinalDate (toOrdinalDate, DayOfYear, Year, fromOrdinalDate)
 import Database.Transaction (transactionM, statement)
 import Control.Lens ((^.))
 import Data.Text (Text)
@@ -37,9 +38,11 @@ handle AuthenticatedUser {ident, institution = Nothing} = pure $ Error (Just 403
           \ write to us to give the access mailto:admin@b-correspondent.app"
 handle AuthenticatedUser {institution = Just ident} = do
   tm <- currentTime
+  let day = toOrdinalDate $ utctDay tm
   let shift = (mod (60 - todMin (timeToTimeOfDay (utctDayTime tm))) 5) * 60
   let upperBound = addUTCTime (fromRational (fromIntegral shift)) tm
-  let lowerBound = addUTCTime (-3600) upperBound
+  let lowerBoundTmp = addUTCTime (-3600) upperBound
+  let lowerBound = mkLowerBound lowerBoundTmp day
   hasql <- fmap (^. katipEnv . hasqlDbPool) ask
   dbResp <- transactionM hasql $ statement getCurrentTimeline (lowerBound, upperBound, ident)
   pure $ withError dbResp $ \xs -> DailyBalanceSheet $ transform xs 
@@ -65,3 +68,7 @@ transform xs =
        let interval = (start, end),
        then group by interval using groupWith
   ] <&> \(xs, (start, end)) -> GapItem start end xs
+
+mkLowerBound :: UTCTime -> (Year, DayOfYear) -> UTCTime
+mkLowerBound tm (day, year) | snd (toOrdinalDate (utctDay tm)) == fromIntegral day = tm
+                            | otherwise = UTCTime (fromOrdinalDate (fromIntegral year) (fromIntegral day)) 0
