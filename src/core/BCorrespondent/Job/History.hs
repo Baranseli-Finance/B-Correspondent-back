3408cf84a8adf616d6bdc82c476f58e4204173dd
Sergey Yakovlev <fclaw007@gmail.com>
2023-10-16 10:00:21 +0300

    history job

--- src/core/BCorrespondent/Job/History.hs
+++ src/core/BCorrespondent/Job/History.hs
@@ -6,16 +6,27 @@
 
 module BCorrespondent.Job.History (refreshMV) where
 
+import qualified BCorrespondent.Statement.History as S (getLastRefreshTm, refreshMV)
 import BCorrespondent.Job.Utils (withElapsedTime)
 import BCorrespondent.ServerM (ServerM)
 import Katip
 import BuildInfo (location)
 import Control.Monad (forever)
 import Control.Concurrent.Lifted (threadDelay)
+import Database.Transaction (transactionM, statement)
+import Katip.Handler (hasqlDbPool, ask)
+import Control.Lens ((^.))
+import Control.Monad.Time (currentTime)
+import Data.Time.Clock (utctDay)
+import Data.Time.Calendar.OrdinalDate (toOrdinalDate)
+import Control.Monad (when)
 
 refreshMV :: Int -> KatipContextT ServerM ()
 refreshMV freq =
-  forever $ do 
+  forever $ do
     threadDelay $ freq * 1_000_000
-    withElapsedTime ($location <> ":refreshMV") $
-      undefined
+    withElapsedTime ($location <> ":refreshMV") $ do
+      currDoY <- fmap (snd . toOrdinalDate . utctDay) currentTime
+      hasql <- fmap (^. hasqlDbPool) ask
+      mvDoY <- transactionM hasql $ statement S.getLastRefreshTm ()
+      when (currDoY > mvDoY) $ transactionM hasql $ statement S.refreshMV ()
--- src/core/BCorrespondent/Statement/History.hs
+++ src/core/BCorrespondent/Statement/History.hs
@@ -1,9 +1,29 @@
-module BCorrespondent.Statement.History (initTimeline) where
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
+
+module BCorrespondent.Statement.History (initTimeline, getLastRefreshTm, refreshMV) where
 
 import BCorrespondent.Transport.Model.Frontend (HistoryDate, encodeHistoryDate)
 import BCorrespondent.Statement.Dashboard (TimelineGapsItem)
 import qualified Hasql.Statement as HS
-import Control.Lens (lmap)
+import Control.Lens (lmap, rmap)
+import Hasql.TH
+import Hasql.Decoders (noResult)
+import Hasql.Encoders (noParams)
 
 initTimeline :: HS.Statement HistoryDate (Either String [TimelineGapsItem])
 initTimeline = lmap encodeHistoryDate undefined
+
+getLastRefreshTm :: HS.Statement () Int
+getLastRefreshTm = 
+  rmap fromIntegral 
+  [singletonStatement|
+    select extract('doy' from max(refresh_time)) :: int 
+    from mv.invoice_and_transaction|]
+
+refreshMV :: HS.Statement () ()
+refreshMV = HS.Statement [uncheckedSql|refresh materialized view mv.invoice_and_transaction|] noParams noResult True
