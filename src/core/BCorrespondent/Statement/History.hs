fcc917316bc5e6d07466fcaa9dcfae7565c8cfdb
Sergey Yakovlev <fclaw007@gmail.com>
2023-10-16 13:21:51 +0300

    history init

--- src/core/BCorrespondent/Statement/History.hs
+++ src/core/BCorrespondent/Statement/History.hs
@@ -29,49 +29,63 @@ initTimeline :: HS.Statement (Int64, Int64, HistoryDate) (Either String (Text, [
 initTimeline = 
   dimap encode (uncurry decode)
   [singletonStatement|
-    select 
-      i.title :: text, 
-      tbl.values :: jsonb[]?
-    from auth.institution as i  
-    left join (
-      select
-        s.institution_id as ident,
-        f.start,
-        f.end,
-        array_agg(
+    with tbl as (
+      select 
+        i.title :: text, 
+        tbl.values :: jsonb[]?
+      from auth.institution as i  
+      left join (
+        select
+          s.institution_id as ident,
+          f.start,
+          f.end,
+          array_agg(
             json_build_object(
-            'start_hour', extract(hour from f.start),
-            'start_minute', extract(minute from f.start),
-            'end_hour', extract(hour from f.end),
-            'end_minute', extract(minute from f.end),
-            'textual_ident', s.textual_view, 
-            'status', s.status,
-            'ident', s.invoice_id,
-            'tm', cast(s.created_at as text) || 'Z',
-            'currency', s.invoice_currency,
-            'amount', s.invoice_amount))
-        filter (where 
+             'start_hour', extract(hour from f.start),
+             'start_minute', extract(minute from f.start),
+             'end_hour', extract(hour from f.end),
+             'end_minute', extract(minute from f.end),
+             'textual_ident', s.textual_view, 
+             'status', s.status,
+             'ident', s.invoice_ident,
+             'tm', cast(s.created_at as text) || 'Z',
+             'currency', s.invoice_currency,
+             'amount', s.invoice_amount))
+          filter (where 
             s.appearance_on_timeline >= f.start 
             and s.appearance_on_timeline < f.end)
-        :: jsonb[] as values    
-      from (
-        select
-          el as start,
-          el + interval '5min' as end
-        from generate_series(
-          make_timestamp($2 :: int, $3 :: int, $4 :: int, 0, 0, 0),
-          make_timestamp($2 :: int, $3 :: int, $4 :: int, 1, 0, 0),
-          interval '5 min') as _(el)) as f
-      cross join (
-        select *
-        from mv.invoice_and_transaction
-        where user_ident = $1 :: int8 and 
-        extract('year' from appearance_on_timeline) = $2 :: int
-        and extract('month' from appearance_on_timeline) = $3 :: int
-        and extract('day' from appearance_on_timeline) = $4 :: int) as s
-      group by s.institution_id, f.start, f.end) as tbl
-    on tbl.ident = i.id 
-    where i.id = $5 :: int8|]
+          :: jsonb[] as values    
+        from (
+          select
+            el as start,
+            el + interval '5min' as end
+            from generate_series(
+            make_timestamp($2 :: int, $3 :: int, $4 :: int, 0, 0, 0),
+            make_timestamp($2 :: int, $3 :: int, $4 :: int, 1, 0, 0),
+            interval '5 min') as _(el)) as f
+        cross join (
+          select *
+            from mv.invoice_and_transaction
+            where user_ident = $1 :: int8 and 
+            extract('year' from appearance_on_timeline) = $2 :: int
+            and extract('month' from appearance_on_timeline) = $3 :: int
+            and extract('day' from appearance_on_timeline) = $4 :: int) as s
+        group by s.institution_id, f.start, f.end) as tbl
+        on tbl.ident = i.id 
+        where tbl.values is not null)
+    select
+      f.title :: text,
+      s.xs :: jsonb[]?
+    from auth.institution as f
+    left join (
+      select
+        f.title :: text,
+        array_agg(distinct s.item) filter(where s.item is not null) :: jsonb[] as xs
+      from tbl as f
+      left join (select unnest(values) as item from tbl) as s on true  
+      group by f.title) as s
+    on f.title = s.title
+    where f.id = $5 :: int8|]
   where 
     encode (user, inst, d) = snocT inst $ consT user $ mapPolyT fromIntegral $ encodeHistoryDate d
     decode title xs = fmap (title,) $ fromMaybe (Right []) $ fmap (sequence . map (A.eitherDecode @TimelineGapsItem . A.encode) . V.toList) xs
