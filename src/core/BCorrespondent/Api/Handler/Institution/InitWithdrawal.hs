f62b086cb7b844c266c44b3fd9449a21ae9209c2
Sergey Yakovlev <fclaw007@gmail.com>
2023-10-18 15:31:46 +0300

    init withdrawal

--- src/core/BCorrespondent/Api/Handler/Institution/InitWithdrawal.hs
+++ src/core/BCorrespondent/Api/Handler/Institution/InitWithdrawal.hs
@@ -3,7 +3,7 @@
 module BCorrespondent.Api.Handler.Institution.InitWithdrawal (handle) where
 
 import BCorrespondent.Api.Handler.Frontend.User.Utils (checkInstitution)
-import BCorrespondent.Statement.Institution (getBalances)
+import BCorrespondent.Statement.Institution (initWithdrawal)
 import BCorrespondent.Api.Handler.Utils (withError)
 import qualified BCorrespondent.Auth as Auth
 import BCorrespondent.Transport.Response (Response)
@@ -17,6 +17,6 @@ handle user =
   checkInstitution user $
     \(_, institution_id) -> do 
       hasql <- fmap (^. katipEnv . hasqlDbPool) ask
-      fmap (flip withError (`InitWithdrawal` [])) $ 
-        transactionM hasql $ 
-          statement getBalances institution_id
+      fmap (flip withError (uncurry InitWithdrawal)) $ 
+        transactionM hasql $
+          statement initWithdrawal institution_id
--- src/core/BCorrespondent/Statement/Institution.hs
+++ src/core/BCorrespondent/Statement/Institution.hs
@@ -3,32 +3,59 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
 
-module BCorrespondent.Statement.Institution (getBalances) where
+module BCorrespondent.Statement.Institution (initWithdrawal) where
 
-import BCorrespondent.Transport.Model.Institution (Balance)
+import BCorrespondent.Transport.Model.Institution (Balance, WithdrawalHistoryItem)
 import BCorrespondent.Transport.Model.Frontend (WalletType (..))
 import qualified Hasql.Statement as HS
 import Control.Lens (dimap)
 import Hasql.TH
 import Data.Int (Int64)
-import Data.Aeson (eitherDecode, encode, toJSON)
+import Data.Aeson (eitherDecode, encode, toJSON, FromJSON, Value)
 import qualified Data.Vector as V
+import Data.Maybe (fromMaybe)
 
-getBalances :: HS.Statement Int64 (Either String [Balance])
-getBalances = 
-  dimap 
-  (\x -> (x, toJSON Debit))
-  (sequence . map (eitherDecode @Balance . encode) . V.toList)
-  [vectorStatement|
-    select 
-      json_build_object(
-      'walletIdent', id,
-      'currency', currency,
-      'amount', amount) :: jsonb
-    from institution.wallet 
-    where institution_id = $1 :: int8 
-    and wallet_type = ($2 :: jsonb) #>> '{}'|]
+initWithdrawal :: HS.Statement Int64 (Either String ([Balance], [WithdrawalHistoryItem]))
+initWithdrawal = 
+  dimap (\x -> (x, toJSON Debit)) decode
+  [singletonStatement|
+    select
+      f.wallets :: jsonb[],
+      s.history :: jsonb[]?
+    from (
+      select
+        institution_id as ident,
+        array_agg(
+          jsonb_build_object(
+           'walletIdent', id,
+           'currency', currency,
+           'amount', amount)) as wallets
+      from institution.wallet
+      where wallet_type = ($2 :: jsonb) #>> '{}'
+      group by institution_id) as f
+    left join (
+      select 
+        s.institution_id as ident,
+        array_agg(
+          jsonb_build_object(
+           'ident', f.id,
+           'currency', s.currency,
+           'amount', f.amount,
+           'withdrawalStatus', f.status,
+           'created', f.created_at)) as history
+      from institution.withdrawal as f 
+      inner join institution.wallet as s
+      on s.id = f.wallet_id
+      where s.wallet_type = ($2 :: jsonb) #>> '{}'
+      group by s.institution_id) as s
+    on f.ident = s.ident    
+    where f.ident = $1 :: int8|]
+  where 
+    transform :: forall a . FromJSON a => V.Vector Value -> Either String [a]
+    transform = sequence . map (eitherDecode @a . encode) . V.toList 
+    decode (xs, ys) = (,) <$> transform @Balance xs <*> fromMaybe (Right []) (fmap (transform @WithdrawalHistoryItem) ys)
