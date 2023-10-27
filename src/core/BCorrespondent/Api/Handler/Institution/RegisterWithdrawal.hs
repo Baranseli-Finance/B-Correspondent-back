b5efe30f5ff00c30b2ddf239e7f51e2b53776cd2
Sergey Yakovlev <fclaw007@gmail.com>
2023-10-27 10:02:16 +0300

    fix

--- src/core/BCorrespondent/Api/Handler/Institution/RegisterWithdrawal.hs
+++ src/core/BCorrespondent/Api/Handler/Institution/RegisterWithdrawal.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module BCorrespondent.Api.Handler.Institution.RegisterWithdrawal (handle) where
 
@@ -6,15 +8,19 @@ import BCorrespondent.Api.Handler.Frontend.User.Utils (checkInstitution)
 import BCorrespondent.Statement.Institution (registerWithdrawal, WithdrawResult (..))
 import qualified BCorrespondent.Auth as Auth
 import BCorrespondent.Api.Handler.Utils (withError)
-import BCorrespondent.Transport.Response (Response)
+import BCorrespondent.Transport.Error (asError)
+import BCorrespondent.Transport.Response (Response (Error))
 import BCorrespondent.Transport.Model.Institution 
        (Withdraw (..), WithdrawResult (..))
 import qualified BCorrespondent.Transport.Model.Institution as I
 import Katip.Handler (KatipHandlerM, katipEnv, hasqlDbPool, ask)
 import Database.Transaction (transactionM, statement)
 import Control.Lens ((^.))
+import Data.Text (Text)
+import GHC.Float (floatToDigits)
 
 handle :: Auth.AuthenticatedUser 'Auth.Writer -> Withdraw -> KatipHandlerM (Response I.WithdrawResult)
+handle user (Withdraw _ amount) | sum (fst (floatToDigits 10 amount)) == 0 = pure $ Error Nothing $ asError @Text "cannot process 0.00"
 handle user (Withdraw ident amount) = 
   checkInstitution user $ \(user_id, _) -> do
     hasql <- fmap (^. katipEnv . hasqlDbPool) ask
