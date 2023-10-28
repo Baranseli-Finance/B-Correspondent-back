b2ef327ee11f4c4596863e1175b49bb6667a4c5c
Sergey Yakovlev <fclaw007@gmail.com>
2023-10-28 12:32:10 +0300

    deploy

--- migration/liquibase_policy_init.sh
+++ migration/liquibase_policy_init.sh
@@ -17,7 +17,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # ============LICENSE_END======================================================
 
-b-correspondent_env_file=$(realpath -s b-correspondent_env)
+b_correspondent_env_file=$(realpath -s b_correspondent_env)
 
 # db_user, db_pass, db, minio_access_key, minio_secret_key
 declare -a keysmap
@@ -26,11 +26,11 @@ idx=0
 while IFS= read -r line || [[ -n "$line" ]]; do
     keysmap[idx]=$line
     (( idx++ ))
-done < "${b-correspondent_env_file}"
+done < "${b_correspondent_env_file}"
 
 /liquibase/liquibase \
     --driver=org.postgresql.Driver \
-    --url=jdbc:postgresql://db:5432/${keysmap[3]} \
+    --url=jdbc:postgresql://db:5432/${keysmap[2]} \
     --changeLogFile=changelog/changelog.xml \
     --username=${keysmap[0]} \
     --password=${keysmap[1]} \
