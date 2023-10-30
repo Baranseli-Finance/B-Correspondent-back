d7612f9585e492eeb18c1ad5952bd9e15dee4e07
Sergey Yakovlev <fclaw007@gmail.com>
2023-10-30 19:12:44 +0300

    deploy

--- deploy/init.sh
+++ deploy/init.sh
@@ -4,7 +4,7 @@ mute500="${MUTE_500:-True}"
 
 env_file=$(realpath -s env.yaml)
 
-b-correspondent_env_file=$(realpath -s b_correspondent_env)
+bcorrespondent_env_file=$(realpath -s bcorrespondent_env)
 front_env_file=$(realpath -s front_env)
 
 # db_user, db_pass, db, minio_access_key, minio_secret_key
@@ -14,7 +14,7 @@ idx=0
 while IFS= read -r line || [[ -n "$line" ]]; do
     keysmap[idx]=$line
     (( idx++ ))
-done < "${b-correspondent_env_file}"
+done < "${bcorrespondent_env_file}"
 
 echo 'launch server..'
 . /home/nix/.nix-profile/etc/profile.d/nix.sh && \
@@ -22,16 +22,14 @@ echo 'launch server..'
     --log-format bar-with-logs \
     --verbose \
     --command \
-    "$PWD/bin/b-correspondent \
+    "$PWD/bin/bcorrespondent \
         --cfg_path deploy/config.yaml \
         --path_to_katip deploy \
         --path_to_jwk deploy/jwk.txt \
         --print_cfg y \
-        --env_path $env_file \
-        --mute500 $mute500 \
+        --minio_access_key ${keysmap[3]} \
+        --minio_secret_key ${keysmap[4]} \
+        --front_env_file_path $front_env_file \
         --b_correspondent_db_user ${keysmap[0]} \
         --b_correspondent_db_pass ${keysmap[1]} \
-        --b_correspondent_database ${keysmap[2]} \
-        --minio_access_key ${keysmap[3]} \
-        --minio_secret_key ${keysmap[4]} \ 
-        --front_env_file_path $front_env_file"
+        --b_correspondent_database ${keysmap[2]}"
