70867214a8a36238741adbebf145a2285f07dd21
Sergey Yakovlev <fclaw007@gmail.com>
2023-11-09 17:40:12 +0300

    elk

--- deploy/elk/setup/entrypoint.sh
+++ deploy/elk/setup/entrypoint.sh
@@ -3,36 +3,36 @@
 set -eu
 set -o pipefail
 
-source "${BASH_SOURCE[0]%/*}"/lib.sh
+# source "${BASH_SOURCE[0]%/*}"/lib.sh
 
-log 'Waiting for availability of Elasticsearch. This can take several minutes.'
+# log 'Waiting for availability of Elasticsearch. This can take several minutes.'
 
-declare -i exit_code=0
-wait_for_elasticsearch || exit_code=$?
+# declare -i exit_code=0
+# wait_for_elasticsearch || exit_code=$?
 
-if ((exit_code)); then
-	case $exit_code in
-		6)
-			suberr 'Could not resolve host. Is Elasticsearch running?'
-			;;
-		7)
-			suberr 'Failed to connect to host. Is Elasticsearch healthy?'
-			;;
-		28)
-			suberr 'Timeout connecting to host. Is Elasticsearch healthy?'
-			;;
-		*)
-			suberr "Connection to Elasticsearch failed. Exit code: ${exit_code}"
-			;;
-	esac
+# if ((exit_code)); then
+# 	case $exit_code in
+# 		6)
+# 			suberr 'Could not resolve host. Is Elasticsearch running?'
+# 			;;
+# 		7)
+# 			suberr 'Failed to connect to host. Is Elasticsearch healthy?'
+# 			;;
+# 		28)
+# 			suberr 'Timeout connecting to host. Is Elasticsearch healthy?'
+# 			;;
+# 		*)
+# 			suberr "Connection to Elasticsearch failed. Exit code: ${exit_code}"
+# 			;;
+# 	esac
 
-	exit $exit_code
-fi
+# 	exit $exit_code
+# fi
 
-sublog 'Elasticsearch is running'
+# sublog 'Elasticsearch is running'
 
 
-curl -u elastic -XPUT 'localhost:9200/_security/user/sonny'  -H 'Content-Type: application/json' -d '{ "password" : "'"KIBANA_SYSTEM_PASSWORD"'", "roles" : [ "kibana_system" ] }'
+curl -u elastic -XPUT 'localhost:9200/_security/user/sonny'  -H 'Content-Type: application/json' -d '{ "password" : "'"$KIBANA_SYSTEM_PASSWORD"'", "roles" : [ "kibana_system" ] }'
 
 curl -u elastic -XPOST 'localhost:9200/security/role/logstash_writer' \
  -H 'Content-Type: application/json' \
