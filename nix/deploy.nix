688af30728ac3de7bd5bcd752efd1f89f7f7353c
Sergey Yakovlev <fclaw007@gmail.com>
2023-11-14 11:15:47 +0300

    psql 16

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -1,7 +1,7 @@
 let
-   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.05.tar.gz") {};
+   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {};
 in
 pkgs.mkShell {
   LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive"; 
-  buildInputs = [ pkgs.postgresql_15 pkgs.lzma pkgs.zlib pkgs.imagemagick pkgs.glibcLocales pkgs.gnumake ];
+  buildInputs = [ pkgs.postgresql_16 pkgs.lzma pkgs.zlib pkgs.imagemagick pkgs.glibcLocales pkgs.gnumake ];
   }
