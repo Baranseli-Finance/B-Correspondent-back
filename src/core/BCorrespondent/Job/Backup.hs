fa5b0c9a2207ca8677bee6378a7d330fcbda5471
Sergey Yakovlev <fclaw007@gmail.com>
2023-11-20 12:43:14 +0400

    backup: parallel mode

--- src/core/BCorrespondent/Job/Backup.hs
+++ src/core/BCorrespondent/Job/Backup.hs
@@ -31,6 +31,7 @@ import System.Directory (getTemporaryDirectory)
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import Database.Transaction (statement, transactionM)
 import Data.Text (Text)
+import qualified Data.Text as T 
 import Data.Foldable (for_)
 import Data.Either.Combinators (whenLeft)
 import Data.String.Conv (toS)
@@ -38,6 +39,7 @@ import Network.HTTP.Client.MultipartFormData (partBS)
 import qualified Request (safeMake, methodPost)
 import Network.HTTP.Types.Header (hAuthorization, hContentType)
 import Data.ByteString (ByteString)
+import qualified Data.ByteString as B (length, splitAt, takeEnd)
 import Crypto.Cipher.Symmetric (twofish128Key, twofish128IV, twofish128)
 import Data.Functor ((<&>))
 import Data.Maybe (fromMaybe)
@@ -46,6 +48,7 @@ import qualified Data.ByteString.Base64 as B64
 import Data.Text.Encoding (decodeUtf8)
 import Data.Time.Clock (getCurrentTime)
 import Data.Time.LocalTime (getCurrentTimeZone, utcToLocalTime, localTimeOfDay, todHour)
+import qualified Control.Parallel.Strategies as Par
 
 
 run :: Int -> KatipContextT ServerM ()
@@ -84,7 +87,7 @@ run freq = do
               resp <- liftIO $ obtainAccessToken mgr googleTokenUrl googleTokenEmail googleTokenPk
               for_ resp $ \(AccessToken token) -> do
                 k <- fmap (^. symmetricKeyBase) ask 
-                let cipheredContent = cryptContent k content
+                let cipheredContent = cryptContent k $ toS content
                 for_ cipheredContent $ \bs -> do
                   let part = [partBS (toS ("/b-correspondent_" <> show tm <> ".sql")) $ toS bs]
                   let hs = 
@@ -107,13 +110,41 @@ getCurrentHour = do
   timezone <- getCurrentTimeZone
   return $ todHour $ localTimeOfDay $ utcToLocalTime timezone now
 
-cryptContent :: ByteString -> String -> Either String Text
-cryptContent bs content = 
+maxSize = 5_000_000 :: Int
+
+cryptContent :: ByteString -> ByteString -> Either String Text
+cryptContent bs content 
+  | B.length content < maxSize = cryptContentImpl bs content
+  | otherwise =
+    let n = B.length content `div` maxSize
+        chunks = mkChunks n content
+        -- less then maxSize therefore we can easily apply to it 
+        -- the brute-force approach mainly cryptContentImpl
+        rest = B.length content `mod` maxSize
+    in do
+         xs <- cryptInPar bs chunks
+         x <- cryptContentImpl bs $ B.takeEnd rest content
+         pure $ T.concat $ xs <> [x]
+
+cryptContentImpl :: ByteString -> ByteString -> Either String Text
+cryptContentImpl bs content = 
   fromMaybe (Left "couldn't make IV from bs") $
     twofish128IV bs <&> \iv -> 
       bimap show (decodeUtf8 . B64.encode) $ 
-        twofish128 (twofish128Key bs) iv $ toS content
+        twofish128 (twofish128Key bs) iv content
 
+mkChunks :: Int -> ByteString -> [ByteString]
+mkChunks n bs = 
+  let (x, ys) = B.splitAt maxSize bs 
+  in go (n - 1) [x] ys
+  where
+    go 0 rs _ = reverse rs
+    go i rs ys = 
+      let (r, ys') = B.splitAt maxSize ys 
+      in go (n - 1) (r : rs) ys'
+
+cryptInPar :: ByteString -> [ByteString] -> Either String [Text]
+cryptInPar bs = sequence . Par.parMap (Par.rparWith Par.rdeepseq) (cryptContentImpl bs)
 
 logError :: Either String a -> KatipContextT ServerM ()
 logError x = whenLeft x $ \error -> $(logTM) ErrorS $ logStr @Text $ $location <> ":run ---> " <> toS error
