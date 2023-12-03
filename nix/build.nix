5d72e12c471638e7469153e14fa3098305bd8300
Sergey Yakovlev <fclaw007@gmail.com>
2023-12-03 14:17:20 +0300

    nix

--- nix/build.nix
+++ nix/build.nix
@@ -40,17 +40,6 @@ mkShell {
     pg_cron
     gnumake
     figlet
-    # 16 1637.0 bzlib-conduit                    > Error: Cabal-simple_9p6GVs8J_3.8.1.0_ghc-9.4.5: Missing dependency on a
-    # 16 1637.0 bzlib-conduit                    > foreign library:
-    # 16 1637.0 bzlib-conduit                    > * Missing (or bad) C library: bz2
-    # 16 1637.0 bzlib-conduit                    > This problem can usually be solved by installing the system package that
-    # 16 1637.0 bzlib-conduit                    > provides this library (you may need the "-dev" version). If the library is
-    # 16 1637.0 bzlib-conduit                    > already installed but in a non-standard location then you can use the flags
-    # 16 1637.0 bzlib-conduit                    > --extra-include-dirs= and --extra-lib-dirs= to specify where it is.If the
-    # 16 1637.0 bzlib-conduit                    > library file does exist, it may contain errors that are caught by the C
-    # 16 1637.0 bzlib-conduit                    > compiler at the preprocessing stage. In this case you can re-run configure
-    # 16 1637.0 bzlib-conduit                    > with the verbosity flag -v3 to see the error messages.
-    bzip2
   ];
   # Configure the Nix path to our own `pkgs`, to ensure Stack-with-Nix uses the correct one rather than the global <nixpkgs> when looking for the right `ghc` argument to pass in `nix/stack-integration.nix`
   # See https://nixos.org/nixos/nix-pills/nix-search-paths.html for more information
--- nix/stack-integration.nix
+++ nix/stack-integration.nix
@@ -10,5 +10,19 @@ haskell.lib.buildStackProject {
   inherit ghc;
   name = "haskell-stack-nix";
   # System dependencies needed at compilation time
-  buildInputs = [ postgresql_16 pg_cron lzma git zlib ];
+  buildInputs = 
+    [ postgresql_16 
+      pg_cron 
+      lzma 
+      git 
+      zlib
+      # 16 1637.0 bzlib-conduit                    > Error: Cabal-simple_9p6GVs8J_3.8.1.0_ghc-9.4.5: Missing dependency on a
+      # 16 1637.0 bzlib-conduit                    > foreign library:
+      # 16 1637.0 bzlib-conduit                    > * Missing (or bad) C library: bz2
+      bzip2
+      #16 848.3 HsOpenSSL                        > Error: setup: Missing dependencies on foreign libraries:
+      #16 848.3 HsOpenSSL                        > * Missing (or bad) header file: openssl/asn1.h
+      #16 848.3 HsOpenSSL                        > * Missing (or bad) C libraries: ssl, crypto
+      openssl
+    ];
 }
