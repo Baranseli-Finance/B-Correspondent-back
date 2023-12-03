81bddf7a80bc1cce51231d5cbaadf5448a393c11
Sergey Yakovlev <fclaw007@gmail.com>
2023-12-03 15:17:19 +0300

    nix

--- nix/deploy.nix
+++ nix/deploy.nix
@@ -4,5 +4,5 @@ in
 with pkgs;
 mkShell {
   LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive"; 
-  buildInputs = [ postgresql_16 lzma zlib glibcLocales gnumake ];
+  buildInputs = [ postgresql_16 lzma zlib glibcLocales gnumake expat ];
 }
