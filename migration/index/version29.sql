2b061533ca825817bca505f684c5a86abbdcfd57
Sergey Yakovlev <fclaw007@gmail.com>
2023-12-11 16:43:46 +0300

    indexes

---
+++ migration/index/version29.sql
@@ -0,0 +1,2 @@
+create index mv_invoice_and_transaction_appearance_on_timeline on mv.invoice_and_transaction (appearance_on_timeline);
+create index institution_invoice_appearance_on_timeline on institution.invoice (appearance_on_timeline);
