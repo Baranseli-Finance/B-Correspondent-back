48cb39b3932d0fa74ba33791bdb5e33dccd0440c
Sergey Yakovlev <fclaw007@gmail.com>
2024-01-14 21:01:57 +0300

    fix

--- src/core/BCorrespondent/Server.hs
+++ src/core/BCorrespondent/Server.hs
@@ -214,15 +214,15 @@ run Cfg {..} = do
           -- ,  Job.Webhook.run
           -- ,  Job.Cache.removeExpiredItems
              Job.Transaction.forward
-          ,  \s -> forever $ threadDelay (s * 1_000_000) >> liftIO (print "thread 1")
-          ,  \s -> forever $ threadDelay (s * 1_000_000) >> liftIO (print "thread 2")
-          ,  \s -> forever $ threadDelay (s * 1_000_000) >> liftIO (print "thread 3")
-          ,  \s -> forever $ threadDelay (s * 1_000_000) >> liftIO (print "thread 4")
-          ,  \s -> forever $ threadDelay (s * 1_000_000) >> liftIO (print "thread 5")
-          ,  \s -> forever $ threadDelay (s * 1_000_000) >> liftIO (print "thread 6")
-          ,  \s -> forever $ threadDelay (s * 1_000_000) >> liftIO (print "thread 7")
-          ,  \s -> forever $ threadDelay (s * 1_000_000) >> liftIO (print "thread 8")
-          ,  \s -> forever $ threadDelay (s * 1_000_000) >> liftIO (print "thread 9")
+          ,  \s -> forever $ threadDelay (s * 1_000_000) >> ($(logTM) InfoS $ ls @T.Text "thread 1")
+          ,  \s -> forever $ threadDelay (s * 1_000_000) >> ($(logTM) InfoS $ ls @T.Text "thread 2")
+          ,  \s -> forever $ threadDelay (s * 1_000_000) >> ($(logTM) InfoS $ ls @T.Text "thread 3")
+          ,  \s -> forever $ threadDelay (s * 1_000_000) >> ($(logTM) InfoS $ ls @T.Text "thread 4")
+          ,  \s -> forever $ threadDelay (s * 1_000_000) >> ($(logTM) InfoS $ ls @T.Text "thread 5")
+          ,  \s -> forever $ threadDelay (s * 1_000_000) >> ($(logTM) InfoS $ ls @T.Text "thread 6")
+          ,  \s -> forever $ threadDelay (s * 1_000_000) >> ($(logTM) InfoS $ ls @T.Text "thread 7")
+          ,  \s -> forever $ threadDelay (s * 1_000_000) >> ($(logTM) InfoS $ ls @T.Text "thread 8")
+          ,  \s -> forever $ threadDelay (s * 1_000_000) >> ($(logTM) InfoS $ ls @T.Text "thread 9")
           ]
 
     asyncXs <- mapM Async.Lifted.async $ zipWith ($) jobXs $ map (jobFrequency +) [1, 3 .. ]
